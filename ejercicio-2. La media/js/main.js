0f4de694d3e2e4ee6e972e299526b9f7b3ccc985
evasanzgue <evasanzgue@gmail.com>
2024-06-09 15:51:33 +0200

    exercises

---
+++ ejercicio-2. La media/js/main.js
@@ -0,0 +1,20 @@
+'use strict'
+
+let numbers = [4, 5, 6, 9, 236, 20] ;
+let acc = 0
+
+for (let i = 0; i <=  numbers.length; i++) {
+    debugger;
+    acc += numbers[i];
+    
+}
+const media = acc / numbers.length;
+console.log(media);
+
+
+//Ejercicio B
+
+function average(numbers) {
+    
+}
+    
---
+++ javascript-bucles-ejer.1/js/main.js
@@ -0,0 +1,12 @@
+'use strick';
+
+let acc = 0;
+
+for (let i = 0 ; i <= 10; i++) {
+    
+    acc += 2
+    console.log(`El resultado es: ${acc}`);
+}
+
+
+
